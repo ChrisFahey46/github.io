fddec93ffbcc0403df05af12ce08ec1919734b03
Chris Fahey <chris_fahey@hotmail.com>
2020-08-27 23:09:31 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function randomValueFromArray(array){
 }
 
 let storyText = ':insertx: :inserty: :insertz: :inserta:.';
-let insertX = ['Tá', 'An bhfuil', 'Bhí', 'An raibh', 'Beidh', 'An mbeidh', 'Bíonn', 'An mbíonn'];
+let insertX = ['Tá', 'An bhfuil', 'Níl', 'Bhí', 'An raibh', 'Ní raibh', 'Beidh', 'An mbeidh', 'Ní bheidh', 'Bíonn', 'An mbíonn', 'Ní bhím'];
 let insertY = ['an t-arán', 'an t-im', 'an t-uachtar', 'an tsubh', 'an císte', 'an cat', 'an madra', 'an bord', 'an chathaoir', 'an geata', 'an ghloine', 'an ceacht', 'an milséan', 'an reoiteog', 'an teilifís', 'an pictiúr', 'an chailc', 'an rothar', 'an charr', 'an punt', 'an t-airgead', 'an mála', 'an bosca', 'an bóthar', 'an tsráid', 'an bhróg', 'an t-urlár', 'an bainne', 'an túll', 'an fear', 'an bhean', 'an páiste', 'an cailín', 'an buachaill', 'an múinteoir'];
 let insertZ = ['tógtha', 'dúnta', 'glanta', 'díolta', 'gearrtha', 'caillte', 'curtha', 'bainte', 'caite', 'briste', 'ólta', 'fágtha', 'dóite', 'nite', 'léite', 'scríofa', 'líonta', 'tugtha', 'ite', 'cloiste', 'beirthe', 'ráite', 'déanta', 'faighte', 'feicthe', 'ceannaithe', 'tosaithe', 'críochnaithe', 'cruinnithe', 'deisithe', 'foghlamtha', 'oscailte', 'ceangailte', 'imeartha' ];
 let insertA = ['agam', 'agat', 'aige', 'aici', 'againn', 'agaibh', 'acu']
